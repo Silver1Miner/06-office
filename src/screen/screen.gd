7f72a6a378ff5995da21bdf9db882cc166dee79a
Your Name <silver1miner@gmail.com>
2021-10-02 12:56:43 -0700

    screen escape function

--- src/screen/screen.gd
+++ src/screen/screen.gd
@@ -6,9 +6,9 @@ var TOP_LIMIT = 60
 var BOTTOM_LIMIT = 340
 var capture_mouse := true
 onready var icon = $screen/icon
+onready var progress = $TextureProgress
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	if icon.connect("icon_pressed", self, "_on_icon_pressed") != OK:
 		push_error("signal connect fail")
 
@@ -16,6 +16,12 @@ func _on_icon_pressed(identity: String) -> void:
 	print(identity + " icon pressed")
 
 func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_R):
+		progress.value += 1
+		if progress.value == 100:
+			print("released")
+			capture_mouse = false
+			progress.value = 0
 	if capture_mouse:
 		if get_global_mouse_position().x < LEFT_LIMIT:
 			warp_mouse(Vector2(LEFT_LIMIT, get_global_mouse_position().y))
